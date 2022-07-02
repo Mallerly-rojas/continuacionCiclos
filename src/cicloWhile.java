6cfc29e4072d949b6484c343712c60ec5c8d4d3e
unknown <Mallerly Rojas Cardenas>
2022-07-02 08:14:29 -0500

    se imprimen los numeros del 1 al 10

--- src/cicloWhile.java
+++ src/cicloWhile.java
@@ -1,2 +1,16 @@
 public class cicloWhi{
+    public static void main(String[] args) {
+        //ciclo whilw
+        // pintar los njumeros del 1 al 10
+        boolean noHaTerminado = true;
+        int numero = 1;
+         while (noHaTerminado){
+             System.out.println("numero: " +numero);
+             numero = numero + 1;
+             if ( numero > 10 ){
+                 noHaTerminado = false;
+
+             }
+         }
+    }
 }

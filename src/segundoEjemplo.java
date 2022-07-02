e4f103c232c4a88b3762419b58ef7736514822c0
unknown <Mallerly Rojas Cardenas>
2022-07-02 08:30:46 -0500

    se crea clase en la que se realiza el ejemplo de como imprimir los numeros del 10 al 1

---
+++ src/segundoEjemplo.java
@@ -0,0 +1,12 @@
+public class segundoEjemplo {
+    public static void main(String[] args) {
+        //escribir los numeros del 10 al 1
+        int numero = 10;
+        while (numero >= 1){
+            System.out.println("numero: " + numero);
+            numero = numero - 1;
+
+        }
+
+    }
+}
